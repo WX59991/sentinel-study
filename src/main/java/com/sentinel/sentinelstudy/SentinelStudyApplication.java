390beebeccbaf030d9621b3dd5b44512b1eed6ec
wangxia <wangxia5991@163.com>
2019-07-31 10:34:56 +0800

    first commit

---
+++ src/main/java/com/sentinel/sentinelstudy/SentinelStudyApplication.java
@@ -0,0 +1,13 @@
+package com.sentinel.sentinelstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelStudyApplication.class, args);
+    }
+
+}
